eeff11d1acddd9dfffef1a14986bf4233fd77f9b
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-10-29 00:32:20 +0200

    chore(all): cleanup

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+import { WebpackConfig } from '@easy-webpack/core';
+declare var _default: ({library, libraryTarget, umdNamedDefine}: {
+    library?: any;
+    libraryTarget?: string;
+    umdNamedDefine?: boolean;
+}) => (this: WebpackConfig) => WebpackConfig;
+export = _default;
